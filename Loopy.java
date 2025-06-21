28c1029f681b2ba2ef3829fea9986a4a69d7a865
Arman Jasuja <arman_jasuja@yahoo.com>
2025-06-21 11:27:44 +0400

    Basic Loop

---
+++ Loopy.java
@@ -0,0 +1,11 @@
+public class Loopy {
+    public static void main(String[] args) {
+        int x = 12;
+
+        while (x > 4) {
+            System.out.println("Still in loop!");
+            System.out.println("The value of x is " + x);
+            x--;
+        }
+    }
+}
